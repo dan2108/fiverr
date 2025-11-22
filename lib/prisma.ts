bfada277703894ce4ff2b3c0274d9e8747da5e30
dan2108 <dan2108@users.noreply.github.com>
2025-11-22 11:44:59 +0000

    Fix Prisma Client import with type-only import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import type { PrismaClient } from '@prisma/client'
+import { PrismaClient as PrismaClientConstructor } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma: PrismaClient = globalForPrisma.prisma ?? new PrismaClientConstructor()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
